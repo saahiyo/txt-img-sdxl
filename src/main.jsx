4c4423fa43cbf1f2d00bce8fe5f3ccfb3d8d12dc
saahiyo <linkg518@gmail.com>
2025-07-08 11:39:07 +0530

    Update Vercel Analytics import in main.jsx to use the correct package path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx'
 import './index.css'
 
